8d66c3e84ee1cf5cf88621b53df00615d58ce941
ZachyDev <mosetizachary001@gmail.com>
2020-06-10 14:14:33 +0300

    implemented node security check

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 // chalk specifies the color of the message when the server starts
 const chalk = require('chalk');
-console.log(chalk.green('Starting dev server...'));
+console.log(chalk.green('Starting app in  dev mode...'));
