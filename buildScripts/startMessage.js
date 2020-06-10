a0439e93caf685b780b04decc19a37173417cc7e
ZachyDev <mosetizachary001@gmail.com>
2020-06-10 15:17:43 +0300

    added babel transpiler

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,2 @@
-// chalk specifies the color of the message when the server starts
-const chalk = require('chalk');
-console.log(chalk.green('Starting app in  dev mode...'));
+import chalk from 'chalk';
+console.log(chalk.green('Starting app in dev mode...'))
