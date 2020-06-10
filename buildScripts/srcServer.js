4ef663a1c9e28705a96322b2dc0b512e24d5ef94
ZachyDev <mosetizachary001@gmail.com>
2020-06-10 13:11:47 +0300

    set up local dev server

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,12 +6,13 @@ const app = express();
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'../src/index.html'));
-})
+});
+
 app.listen(port,function(err){
-    if(err){
+    if (err) {
         console.log(err);
     }
-    else{
+    else {
         open('http://localhost:' + port);
     }
 })
