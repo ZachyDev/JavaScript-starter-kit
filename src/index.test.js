50d41e3e8e38548df05511bf00d74bceeeb2d651
ZachyDev <mosetizachary001@gmail.com>
2020-06-17 01:26:02 +0300

    Started unit testing using Mocha and an assertion library called Chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+// import assertion library
+import { expect, should } from 'chai';
+
+describe('Our first test',() => {
+    it('Should pass',() => {
+        expect(true).to.equal(true);
+    });
+});
