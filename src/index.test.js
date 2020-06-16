9a60c55ad7aff5c25666ea51fa808ca2ff5c04b5
ZachyDev <mosetizachary001@gmail.com>
2020-06-17 01:50:00 +0300

    First test using Mocha and Chai

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,22 @@
 // import assertion library
-import { expect, should } from 'chai';
-
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 describe('Our first test',() => {
     it('Should pass',() => {
         expect(true).to.equal(true);
     });
 });
+
+// testing index.html for Hello World!
+describe('testing index.htm file',() => {
+    it('should have the text Hello World!',(done) => {
+        const index = fs.readFileSync('./src/index.html','utf-8');
+        jsdom.env(index,(err,window) => {
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal('Hello World!');
+            done();
+            window.close();
+        })
+    })
+})
