75dcb5b0efa035be50c50373ce283243fe5018a7
ZachyDev <mosetizachary001@gmail.com>
2020-06-18 00:12:28 +0300

    test files

--- src/index.test.js
+++ src/index.test.js
@@ -1,23 +1,23 @@
 import { expect } from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
-// start the test
-describe('testing 4 + 4',() => {
-    it('should return 8',() => {
-        expect(4 + 4).to.equal(8);
-    });
+
+// start the tests
+describe('we expect 10 + 10 to equal 20',() => {
+    it('should add 10 + 10 then return 20',() => {
+        expect(10+10).to.equal(20);
+    })
 })
 
-// second test
-
-// testing whether the correct format has been matched
-describe('The test should match Hello World!',() => {
-    it('should return Hello World!',(done)=> {
-        const index = fs.readFileSync('./src/index.html','utf-8');
-        jsdom.env(index,(err,window) => {
+// check the matching text
+describe('Testing index.html',() => {
+    it('the h1 tag should have Hello World!',(done) => {
+        const indexFile = fs.readFileSync('./src/index.html','utf-8');
+        jsdom.env(indexFile,(err,window) => {
             const h1 = window.document.getElementsByTagName('h1')[0];
             expect(h1.innerHTML).to.equal('Hello World!');
             done();
+            window.close();
         })
     })
 })
